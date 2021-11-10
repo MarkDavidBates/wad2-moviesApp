86b4472f09fcbf65de6bd3ee6b8f3951030d495d
Mark Bates <markbates.339@gmail.com>
2021-11-10 14:28:54 +0000

    fixed upcoming movies page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -12,13 +12,27 @@ export const getMovies = () => {
   });
 };
 
-  export const getUpcomingMovies = () => {
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+/**  export const getUpcomingMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     )
       .then(res => res.json())
       .then(json => json.results);
-  };
+  }; */
   
   export const getMovie = (args) => {
     console.log(args)
