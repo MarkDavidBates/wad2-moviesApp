64c2a6fc479f82f38da1f19d44a11b5ee58a2d64
Mark Bates <markbates.339@gmail.com>
2021-12-30 17:47:53 +0000

    added extra header code to the getMovies method to properly display movies. Added the private routes for the favourites page that redirects to a new login page, all with appropriate contexts and components

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -2,6 +2,9 @@ export const getMovies = () => {
   return fetch(
     //`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     `/api/movies?page=1&limit=11`
+    ,{headers:{
+      'Authorization':window.localStorage.getItem('token')
+    }}
     ).then((response) => {
     if (!response.ok) {
       throw new Error(response.json().message);
